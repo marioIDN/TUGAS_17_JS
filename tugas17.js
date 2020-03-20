cb8fa7c861ebc0c1b15366184fc6a825a5f5c332
Mardionocs <mardionoputra19>
2020-03-21 02:35:35 +0700

    TUGAS 17 JS
    Tugas dari Niomic

---
+++ tugas17.js
@@ -0,0 +1,15 @@
+function Tugas17() {
+    var kalimat1 = "Saya BeLajar bahaSa peMrograman deNgan khUsuk";
+    var kalimat2 = "Allem Sedang MemAncinG ikaN di SuNgai, DIA MEndapat ikaN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+    console.log(kalimat1.toLowerCase());
+    console.log(kalimat1.toUpperCase());
+    console.log(kalimat2.toLowerCase());
+    console.log(kalimat2.toUpperCase());
+
+    console.log("");
+    var kalimat3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    for (var i = 0; i < 6; i += 1) {
+      console.log(kalimat3.charCodeAt(i));
+    }
+}
+Tugas17()
